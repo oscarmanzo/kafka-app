370085d5a7acc0978c4e2679ed6a5a1ff823908a
Oscar Manzo <oscar.manzo.c@gmail.com>
2020-03-02 12:32:48 -0600

    init commit

---
+++ src/test/java/com/kafka/course/kafkaapp/KafkaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.course.kafkaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
